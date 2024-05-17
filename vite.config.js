ead53ec169cd18ff8b9668b987444dba1a89d31d
airgirl2305 <kheda.askhabova@gmail.com>
2024-05-17 23:19:38 +0300

    Vue router is installed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJSX from '@vitejs/plugin-vue-jsx'
+import vueRouter from '@vitejs/plugin-vue-router'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJSX()],
+  plugins: [vue(), vueJSX(), vueRouter()],
 })
